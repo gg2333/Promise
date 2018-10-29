cf2b7c74e06c55edb8592c79774a3700d58dd853
master_u <905588195@qq.com>
2018-10-29 13:31:36 +0800

    exo

--- app/src/main/java/com/master/promise/MainActivity.java
+++ app/src/main/java/com/master/promise/MainActivity.java
@@ -1,7 +1,11 @@
 package com.master.promise;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.master.exo290.PlayerActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        View playView = findViewById(R.id.play_view);
+        playView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PlayerActivity.start(view.getContext());
+            }
+        });
+
     }
 }
